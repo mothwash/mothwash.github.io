5b96f69ca3fb6931d1965334af0340545058a46e
mothwash <mothwash@users.noreply.github.com>
2020-07-01 11:37:18 -0400

    m4bl+bds
    traded shop for activism - fight the power

--- js/main.js
+++ js/main.js
@@ -324,6 +324,70 @@ function magnifylong(imgID, zoom) {
 }
 
 
+function magnifylonger(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  /* Create magnifier glass: */
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass-longer");
+
+  /* Insert magnifier glass: */
+  img.parentElement.insertBefore(glass, img);
+
+  /* Set background properties for the magnifier glass: */
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  /* Execute a function when someone moves the magnifier glass over the image: */
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /* Prevent any other actions that may occur when moving over the image */
+    /*
+    e.preventDefault();
+    */
+    /* Get the cursor's x and y positions: */
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /* Prevent the magnifier glass from being positioned outside the image: */
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /* Set the position of the magnifier glass: */
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /* Display what the magnifier glass "sees": */
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /* Get the x and y positions of the image: */
+    a = img.getBoundingClientRect();
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+
+
 function selectanswer(answer) {
   // declar variables
   var qcontainer = answer.parentElement;
@@ -405,7 +469,8 @@ function openMobileNav() {
   var headermenu = document.getElementsByClassName('header-container')[0];
   var row = document.createElement('div');
   var column = document.createElement('div');
-  var shoplink = document.createElement('a');
+  //var shoplink = document.createElement('a');
+  var activlink = document.createElement('a');
   var poemslink = document.createElement('a');
   var musiclink = document.createElement('a');
 
@@ -420,10 +485,17 @@ function openMobileNav() {
     column.classList.add('col-sm-12');
     column.classList.add('mobile-menu-items');
 
-    shoplink.innerText = 'Shop';
-    shoplink.setAttribute('href', 'mobileshop.html');
-    shoplink.classList.add('mobile-menu-item');
-    column.appendChild(shoplink);
+    //shoplink.innerText = 'Shop';
+    //shoplink.setAttribute('href', 'mobileshop.html');
+    //shoplink.classList.add('mobile-menu-item');
+    //column.appendChild(shoplink);
+    //var breaktag = document.createElement('br');
+    //column.appendChild(breaktag);
+
+    activlink.innerText = 'Activism';
+    activlink.setAttribute('href', 'activism.html');
+    activlink.classList.add('mobile-menu-item');
+    column.appendChild(activlink);
     var breaktag = document.createElement('br');
     column.appendChild(breaktag);
 
