886fc35e3af274fa223e9da13723fa92a07f8175
mothwash <62674548+mothwash@users.noreply.github.com>
2021-11-20 09:39:17 -0600

    new song and play button patch

--- js/pages/musicdata.js
+++ js/pages/musicdata.js
@@ -1,14 +1,20 @@
 let songs = [
     {
-        name: 'shimmore (snowmigrant)',
-        path: '../img/music/shimmore_snowmigrant.wav',
+        name: 'compass stage (lo stresso padre celeste)',
+        path: '../img/music/compass_stage_draft03.wav',
         artist: 'mothwash',
-        cover: '../img/music/shimmoresnowmigrant.jpeg'
+        cover: '../img/music/compassstage.jpeg'
     },
     {
         name: 'cuckoos turn 2 clocks 4 winter',
         path: '../img/music/cuckoosturn2clocks4winter_draft2.wav',
         artist: 'mothwash',
         cover: '../img/music/cuckoos.JPG'
-    }
+    },
+    {
+        name: 'shimmore (snowmigrant)',
+        path: '../img/music/shimmore_snowmigrant.wav',
+        artist: 'mothwash',
+        cover: '../img/music/shimmoresnowmigrant.jpeg'
+    },
 ]
