500fdf5246ef6ec442c6f899fd6f1c60b087ef63
mothwash <62674548+mothwash@users.noreply.github.com>
2022-05-29 12:36:50 -0500

    newMusic

--- js/pages/musicdata.js
+++ js/pages/musicdata.js
@@ -1,15 +1,111 @@
 let songs = [
     {
-        name: 'when i arrived (demo)',
-        path: '../img/music/wheniarrived_draft02.wav',
+        name: 'coyote embouchure',
+        path: '../img/music/coyoteembouchureV01.wav',
         artist: 'mothwash',
-        cover: '../img/music/wheniarrived.JPG'
+        cover: '../img/music/coyoteembouchure.jpg'
     },
     {
-        name: 'cuckoos turn 2 clocks 4 winter',
-        path: '../img/music/cuckoosturn2clocks4winter_draft2.wav',
-        artist: 'mothwash',
-        cover: '../img/music/cuckoos.JPG'
+        name: 'foster home foam',
+        path: '../img/music/sciontific_method/fosterhomefoam.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: 'aphantasia',
+        path: '../img/music/sciontific_method/aphantasia.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: "rainbow's lullaby",
+        path: '../img/music/sciontific_method/rainbowslullaby.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: "spicy peach porpoise",
+        path: '../img/music/sciontific_method/spicypeachporpoise.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: "ekwus",
+        path: '../img/music/sciontific_method/ekwus.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: "cavern ruffles",
+        path: '../img/music/sciontific_method/cavernruffles.wav',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: "parking garage",
+        path: '../img/music/sciontific_method/parkinggarage_v01.mp3',
+        artist: 'sciontific method',
+        cover: '../img/music/sciontific_method/quarrycliner.jpg'
+    },
+    {
+        name: '__ilkicould__',
+        path: '../img/music/__ilkicould__.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'SUNdrythoughts',
+        path: '../img/music/sundrythoughts.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'todeathdoiowe',
+        path: '../img/music/todeathdoiowe.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'cmyk',
+        path: '../img/music/cmyk.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'tidied overcast',
+        path: '../img/music/tidied_overcast.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: '2allwelost & willneverlose',
+        path: '../img/music/2allwelost&willneverlose.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'mars_drowns',
+        path: '../img/music/mars_drowns.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'mythunder fingernails',
+        path: '../img/music/mythunderfingernails.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'light rail through olddreams',
+        path: '../img/music/lightrailthrougholddreams.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
+    },
+    {
+        name: 'sunlit',
+        path: '../img/music/sunlit.wav',
+        artist: 'd.v. bones & balloons',
+        cover: '../img/music/dvbonesballoons_fairymafiaCOVER.jpeg'
     },
     {
         name: 'shimmore (snowmigrant)',
@@ -18,15 +114,9 @@ let songs = [
         cover: '../img/music/shimmoresnowmigrant.jpeg'
     },
     {
-        name: 'compass stage (lo stresso padre celeste)',
-        path: '../img/music/compass_stage_draft03.wav',
+        name: 'cuckoos turn 2 clocks 4 winter',
+        path: '../img/music/cuckoosturn2clocks4winter_draft2.wav',
         artist: 'mothwash',
-        cover: '../img/music/compassstage.jpeg'
-    },
-    {
-        name: 'coyote embouchure',
-        path: '../img/music/coyoteembouchureV01.wav',
-        artist: 'mothwash',
-        cover: '../img/music/coyoteembouchure.jpg'
+        cover: '../img/music/cuckoos.JPG'
     }
 ]
