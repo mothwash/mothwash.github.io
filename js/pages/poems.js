2e12421b44f3c88ddc9b6cb2433a027257fe317a
mothwash <62674548+mothwash@users.noreply.github.com>
2022-12-26 21:03:03 -0600

    poem_shuffle
    new poem shuffle button

--- js/pages/poems.js
+++ js/pages/poems.js
@@ -46,6 +46,34 @@ function filterPoems4() {
 }
 
 
+function hidethispoem(poemcontainer, id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function showthispoem(poemcontainer, id) {
+  document.getElementById(id).style.display = "";
+}
+
+
+function filterPoems5() {
+  // declare variables
+  var input = document.getElementById('searchinput');
+  var poemcontainer = document.getElementById('poemset');
+  input = input.value.toUpperCase();
+  for (count=0; count<allpoems.length; count++) {
+    poemdict = allpoems[count];
+    if (poemdict.title.toUpperCase().indexOf(input) == -1 && poemdict.keywords.toUpperCase().indexOf(input) == -1 && poemdict.poem.toUpperCase().indexOf(input) == -1) {
+      var id = count.toString();
+      hidethispoem(poemcontainer, id);
+    } else {
+      var id = count.toString();
+      showthispoem(poemcontainer, id);
+    }
+  }
+}
+
+
+
 function uncollapse3(poemindex) {
   var allp = document.getElementsByTagName('p')
   var n;
@@ -68,3 +96,72 @@ function uncollapse3(poemindex) {
   //  poembr[j].style.display = "block";
   //}
 }
+
+
+function addpoems() {
+  var poemcontainer = document.getElementById('poemset')
+  console.log("this is the start of addpoems");
+  console.log("there are ",allpoems.length," poems to add")
+  for(count=0; count<allpoems.length; count++) {
+    poemdict = allpoems[count];
+    //create elements and append them to poemcontainer
+    var poem_li_elem = document.createElement('li');
+    var id = count.toString();
+    poem_li_elem.setAttribute('id',id);
+    poem_li_elem.setAttribute('onclick','uncollapse3(this.id)');
+    poem_li_elem.textContent = poemdict.title;
+    poem_ul_elem = document.createElement('ul');
+    poem_a_elem = document.createElement('a');
+    poem_a_elem.setAttribute('href','#');
+    poem_a_elem.classList.add('keywords');
+    poem_a_elem.textContent = poemdict.keywords;
+    poem_ul_elem.appendChild(poem_a_elem);
+    poem_ul_elem.innerHTML = poemdict.poem;
+    poem_li_elem.appendChild(poem_ul_elem);
+    //console.log("poem_li_elem is: ",poem_li_elem)
+    poemcontainer.appendChild(poem_li_elem);
+  };
+}
+
+function removepoems() {
+  var poemcontainer = document.getElementById('poemset');
+  var poem_li_elems = poemcontainer.getElementsByTagName('li');
+  console.log("this is the start to removing poems");
+  console.log("  there are ",poem_li_elems.length," poems to remove");
+  console.log("the first is: ",poem_li_elems[0]);
+  var count = 0
+  counter = poem_li_elems.length
+  while (count < counter) {
+    poem_li_elems[0].remove();
+    //console.log("next up: ",poem_li_elems[0])
+    count += 1;
+    //console.log("count is: ",count)
+  };
+}
+
+
+
+$(document).ready(function() {
+
+  const poemlistelems = document.getElementsByTagName('li');
+  const allpoemselem = document.getElementById('poemset')
+  const shuffle_btn = document.querySelector(".shuffle-btn")
+
+  shuffle_btn.addEventListener('click', function(e) {
+    removepoems()
+
+    var m = allpoems.length, t, i;
+    // shuffle poems
+    while (m) {
+
+      i = Math.floor(Math.random() * m--);
+
+      t = allpoems[m]
+      allpoems[m] = allpoems[i]
+      allpoems[i] = t
+
+    }
+    addpoems()
+  })
+
+});
