565befd32e8ad05048be40e799c847f79cad9baa
mothwash <62674548+mothwash@users.noreply.github.com>
2025-02-15 08:24:45 -0600

    new matters - zine one thing

---
+++ js/pages/matters.js
@@ -0,0 +1,396 @@
+let things = [
+  {
+    id: 0,
+    thing: "check in with each of your beloveds based on your current conditions and communicate with love to each other what you envision for the world you want to build?"
+  },
+  {
+    id: 1,
+    thing: "make people soup and do not stop inviting them over for soup!"
+  },
+  {
+    id: 2,
+    thing: "Build a support network. Join with like-minded people and organize for quality over quantity; a few devoted comrades can go further than a large and dispassionate group. Make art about it. Your support network, the love of your friends and family, can always be broader; build it bigger, with care and intentionality. Make more art about it.",
+    gift: ""
+  },
+  {
+    id: 3,
+    thing: "Talk to people and ask how they’re feeling" ,
+    gift: ""
+  },
+  {
+    id: 4,
+    thing: "distribute literature",
+    gift: ""
+  },
+  {
+    id: 5,
+    thing: "organize a study group",
+    gift: ""
+  },
+  {
+    id: 6,
+    thing: "put up stickers",
+    gift: ""
+  },
+  {
+    id: 7,
+    thing: "disperse seed bombs together",
+    gift: "With every loving bond we forge, and all the new art we make, we divorce ourselves a little more from the demons that haunt us — hopelessness, irony, and complacency — and find sparks of possibility. Try, fail, and try again and again."
+  },
+  {
+    id: 8,
+    thing: "Buy, accumulate, or otherwise procure soon-hard-to-access supplies related to bodily autonomy, and save it for yourself and others in case it’s needed later. Set up a soon-hard-to-access supplies related to bodily autonomy in your community.",
+    gift: ""
+  },
+  {
+    id: 9,
+    thing: "Write letters to people in prison and detention, send them books, and/or do jail support and solidarity for those facing state repression in your communities",
+    gift: "Act in ways that thwart carceral logics in your responses to conflict and harm as well as your day-to-day relations with others. Remember, there are no prisons or cops in olam ha-ba (the world to come)."
+  },
+  {
+    id: 10,
+    thing: "Make art and display it in public. Draw, paint, or write a colorful sign about your dreams, your hopes for a better world, or to celebrate something that you love about this one.",
+    gift: "It doesn’t matter if you don’t think of yourself as an artsy type. If you can, get together with others to do this; share art materials, space, and ideas. Wheat paste (or wallpaper paste or glue) your finished work in public — somewhere you and others will see it when going about your daily lives. You’ve now made a material change to your surroundings. It will make people smile. It will make people feel less alone. It will make visible your resistance as well as visions. It also won’t last forever. Nothing does. You can always make more."
+  },
+  {
+    id: 11,
+    thing: "Learn a new language and schedule mutual practice sessions with others studying your language",
+    gift: "strengthen global solidarity with those who share your values"
+  },
+  {
+    id: 12,
+    thing: "Reach out to other people (or collectives, projects, etc.) in other parts of the world who you share affinity with — Jews and Muslims, dispossessed and displaced people, anarchists and queers, and so on — and see if there's anything you can collaborate on",
+    gift: ""
+  },
+  {
+    id: 13,
+    thing: "Seek out the stories of people who fought or fled authoritarian regimes in the past and present; learn from their experiences, and engage in discussions about our current challenges and a diversity of tactics to address them.",
+    gift: ""
+  },
+  {
+    id: 14,
+    thing: "Learn new skills, share them, and help others learn new skills toward everything we need and desire — everything for everyone, and what’s more for free",
+    gift: ""
+  },
+  {
+    id: 15,
+    thing: "Learn to be a medic",
+    gift: ""
+  },
+  {
+    id: 16,
+    thing: "Learn to be a facilitator",
+    gift: ""
+  },
+  {
+    id: 17,
+    thing: "Learn to be a birth and death doula",
+    gift: ""
+  },
+  {
+    id: 18,
+    thing: "Learn to be an electrician",
+    gift: ""
+  },
+  {
+    id: 19,
+    thing: "Learn to be a filmmaker",
+    gift: ""
+  },
+  {
+    id: 20,
+    thing: "Learn to be a mediator",
+    gift: ""
+  },
+  {
+    id: 21,
+    thing: "Learn to be a writer",
+    gift: ""
+  },
+  {
+    id: 22,
+    thing: "Learn to be a researcher",
+    gift: ""
+  },
+  {
+    id: 23,
+    thing: "Learn how to stop bleeds",
+    gift: ""
+  },
+  {
+    id: 24,
+    thing: "Learn how to plant gardens",
+    gift: ""
+  },
+  {
+    id: 25,
+    thing: "Learn legal defenses that help communities",
+    gift: ""
+  },
+  {
+    id: 26,
+    thing: "Learn how to build houses",
+    gift: ""
+  },
+  {
+    id: 27,
+    thing: "Learn how to purify water",
+    gift: ""
+  },
+  {
+    id: 28,
+    thing: "Learn how to repair cars",
+    gift: ""
+  },
+  {
+    id: 29,
+    thing: "Learn carpentry",
+    gift: ""
+  },
+  {
+    id: 30,
+    thing: "Learn how to fix appliances",
+    gift: ""
+  },
+  {
+    id: 31,
+    thing: "Learn how to make composting toilets",
+    gift: ""
+  },
+  {
+    id: 32,
+    thing: "Learn how to craft zines",
+    gift: ""
+  },
+  {
+    id: 33,
+    thing: "Learn how to sew clothing",
+    gift: ""
+  },
+  {
+    id: 34,
+    thing: "Learn to cook for crowds",
+    gift: ""
+  },
+  {
+    id: 35,
+    thing: "Learn how to help people find refuge",
+    gift: ""
+  },
+  {
+    id: 36,
+    thing: "Learn how to calm the nervous systems of friends and strangers",
+    gift: ""
+  },
+  {
+    id: 37,
+    thing: "Learn how to set up digital and cyber security",
+    gift: ""
+  },
+  {
+    id: 38,
+    thing: "Learn how to access medicine",
+    gift: ""
+  },
+  {
+    id: 39,
+    thing: "Set up peer support networks",
+    gift: "Feel your emotions. Do not sublimate them. Feel them and remember that this connects us to everyone who has ever despaired. Feel them with others."
+  },
+  {
+    id: 40,
+    thing: "Set up weekend-long care clinics",
+    gift: ""
+  },
+  {
+    id: 41,
+    thing: "Set up emotional skills sharing sessions",
+    gift: ""
+  },
+  {
+    id: 42,
+    thing: "sip herbal tea, and reciprocally warm each others’ hearts, even if temporarily",
+    gift: ""
+  },
+  {
+    id: 43,
+    thing: "Learn about and begin to practice alternative decision-making structures and group processes that have served those who got shit done in the past. Practice good processes that are cut from the cloth that the Zapatistas refer to as buen gobierno (good governance, or self-governance).",
+    gift: ""
+  },
+  {
+    id: 44,
+    thing: "Learn about Zapatista autonomous communities, Chéran, Rojava, and many other examples of self-governance, past and present, as inspiration as well as horizons to work toward.",
+    gift: ""
+  },
+  {
+    id: 45,
+    thing: "Gather and distribute free tools for preventive health and diagnostic health as a baseline toward building a more generalized harm reduction crew that can gather and distribute, for example, masks, Naloxone, fentanyl test strips, COVID tests, clean needles, condoms, and lube",
+    gift: ""
+  },
+  {
+    id: 46,
+    thing: "Normalize illness, other health protections, and additional ways of taking reciprocal care of each other",
+    gift: ""
+  },
+  {
+    id: 47,
+    thing: "Go to outdoor events (or mask up for indoor ones) to table and share pamphlets on collective virus safety and harm reduction.",
+    gift: ""
+  },
+  {
+    id: 48,
+    thing: "Join in fascist watches and cop watches, or start them in your neighborhood or city.",
+    gift: "Prepare forward for community self-defense, which can come in many shapes and sizes."
+  },
+  {
+    id: 49,
+    thing: "If you care for a child or children, work with one or many other caregivers to create a mutual aid group if there isn’t one already!",
+    gift: ""
+  },
+  {
+    id: 50,
+    thing: "Plan weekly or biweekly meetups with locations that support childcare in mind (such as a park), and share needs and resources.",
+    gift: ""
+  },
+  {
+    id: 51,
+    thing: "Revive the concept and practice of kassi, the mutual aid funds/networks that used to keep neighbors afloat and supported in eastern European shtetls. Borrow from your own ancestral traditions/histories of mutual aid to build real-life community by strengthening relations with your neighbors and comrades for the days and years to come.",
+    gift: ""
+  },
+  {
+    id: 52,
+    thing: "Paint murals to honor lost friends and comrades.",
+    gift: ""
+  },
+  {
+    id: 53,
+    thing: "Make rituals part of your resistance, queering and self-organizing them in collectivity with others. Take those rituals out into your community — by a river, on a street corner, at a DIY space or radical bookfair, during a forest defense or as a direct action.",
+    gift: "Take time to mourn your losses and grieve your dead — as inseparable from fighting and organizing for the living; as part and parcel of mending the world and ourselves. Lean on the deep wisdom of grief rituals that have sustained life for millennia, such as saying Kaddish for the dead or doing shiva after a loss."
+  },
+  {
+    id: 54,
+    thing: "Feed people for free. Look for a Food Not Bombs or Coffee Not Cops chapter, or similar nonhierarchical mutual aid project near you",
+    gift: ""
+  },
+  {
+    id: 55,
+    thing: "get in touch, and join in collecting ingredients for, cooking, and/or serving a meal",
+    gift: ""
+  },
+  {
+    id: 56,
+    thing: "If there's nothing in your area, organize a free picnic; put up posters and encourage everyone to come — and optionally, bring a dish.",
+    gift: "Talking to the people you share the food with is important; do this if you can. Notice the moment when someone comes to understand that food can be good and free and shared without restrictions, obligations, eligibility criteria, or expectations; this means that things don't have to be the way they are"
+  },
+  {
+    id: 57,
+    thing: "If a friend or someone you know is having suicidal thoughts and reaches out to you, offer to drop everything and be present with them.",
+    gift: "Small acts of peer support can make an enormous difference; think of yourself as a “tourniquet” for them when they most need it. You can hold space for them, for instance; don’t make it about you or act scared but instead simply allow them to share feelings, especially without fear of the cops being called. Or keep them company and help look after their basic needs that day. Or let friends and other people you trust know in advance that they can call you in these kinds of situations, and that you’ll take a weapon away from them for as long as needed if they ask."
+  },
+  {
+    id: 58,
+    thing: "Organize a stoop or porch sale with a few other households, or even a regular stoop or porch sale, and use the funds to cover material needs for solidarity efforts, such as abortion or bail funds, or for gender-affirming surgery or aiding folks during a rent strike.",
+    gift: ""
+  },
+  {
+    id: 59,
+    thing: "Ask yourself: What time and materials could I easily donate that would have an exponential effect and allow me to meet and organize with friends and neighbors in my community?",
+    gift: "Rather than a personal responsibility or charity, fundraising becomes a way of building deeper networks of care and connection"
+  },
+  {
+    id: 60,
+    thing: "Hold game nights",
+    gift: "Engage in play with others as a gateway to imagining other worlds and experimenting with getting there while cultivating camaraderie and goodwill."
+  },
+  {
+    id: 61,
+    thing: "Invent your own versions of group “sports” such as capture the flag, tag, and soccer, and gather folks in a park to make riotously merry.",
+    gift: ""
+  },
+  {
+    id: 62,
+    thing: "Self-organize a queer Purim spiel or other DIY theatrics, and simply be silly (and/or use your performances to make fun of the social order and dream up ways it might tumble).",
+    gift: "Add playfulness to your banners, events, organizing, and actions."
+  },
+  {
+    id: 63,
+    thing: "Slow down.",
+    gift: "Heed Jewish wisdom: days and hours of rest are sacred. Heed disabled wisdom: your work is completely irrelevant to your worth! Your ancestors began weaving unfucked social fabrics and burning down fucked-up ones before you were a glimmer in their eye. Descendants to come will be weaving and unfucking and burning still. What can we even weave with only weak threads connecting us? Trust takes years. Any faster, and conflict rends our fabric like kri’ah, the Jewish ritual act of tearing cloth in mourning. So come fascism or liberation: weave slow, take sabbaticals, feast on kugel, and sing with your comrades down by the river."
+  },
+]
+
+
+
+
+
+
+$(document).ready(function() {
+  let currthing = 0
+
+  function setthing(i) {
+    var onething_elem = document.querySelector('#onething');
+    let onething = things[i];
+
+    onething_elem.innerHTML = onething.thing;
+    currthing = i
+
+    checkgift(currthing)
+
+  }
+
+  function shufflethings() {
+    var m = things.length, t, i;
+    while (m) {
+
+      i = Math.floor(Math.random() * m--);
+
+      t = things[m]
+      things[m] = things[i]
+      things[i] = t
+
+    }
+    setthing(0);
+  }
+
+  function checkgift(currthing) {
+    var gift = things[currthing].gift
+    if (gift != "") {
+      var gift_elem = document.querySelector('#gifts');
+      var new_gift = document.createElement('h4');
+      new_gift.textContent = gift;
+      new_gift.classList.add('gift-class');
+
+      var giftchildren = gift_elem.children;
+      console.log('giftchildren: ',giftchildren)
+      if (giftchildren.length > 0) {
+        gift_elem.insertBefore(new_gift ,giftchildren[0]);
+      } else {
+        gift_elem.append(new_gift);
+      }
+    }
+  }
+
+  function nextthing() {
+    var inlist = null;
+    var activething = document.querySelector('#onething');
+    var activething = activething.children
+
+    //while (inlist == null) {
+      //inlist = checkactive(activething, currthing+1, inlist)
+      if(currthing >= things.length - 1){
+          currthing = 0;
+      } else{
+          currthing++;
+      }
+    //}
+    setthing(currthing);
+  }
+
+  // on document start
+  shufflethings();
+
+  const nextthing_elem = document.querySelector('#nextthing');
+  nextthing_elem.addEventListener('click', nextthing)
+
+
+});
